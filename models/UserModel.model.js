d69b42002a2ad6dbafa029b2c234f47218c8957e
G-Dharani-raj <gdharaniraj0@gmail.com>
2023-03-04 22:28:45 +0530

    removed expiry date

--- models/UserModel.model.js
+++ models/UserModel.model.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const userSchema = mongoose.Schema({
 	name: { type: String, required: true },
 	score: { type: Number, required: true, default: 0 },
-	expireAt: { type: Date, expires: 259200, default: Date.now },
 });
 
 const UserModel = mongoose.model("user", userSchema);
